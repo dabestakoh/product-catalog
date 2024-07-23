22c4a193a421d391c91b04f96290f39215085fed
Marlon Cuevas <marloncuevas1@gmail.com>
2024-07-24 02:51:15 +0800

    uploaded default image and minor UI fixes for search and no results

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mockApiHost: `${window.location.protocol}//${window.location.origin}:3000`,
-  defaultImage: "https://static.vecteezy.com/system/resources/thumbnails/022/059/000/small_2x/no-image-available-icon-vector.jpg"
+  defaultImage: "/assets/images/no-image.png"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   mockApiHost: `${window.location.protocol}//${window.location.hostname}:3000`,
-  defaultImage: "https://static.vecteezy.com/system/resources/thumbnails/022/059/000/small_2x/no-image-available-icon-vector.jpg"
+  defaultImage: "/assets/images/no-image.png"
 };
